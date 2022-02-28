38a687337d817bb00fd21b4f9f6b03339634053b
Saikumar Iyer <si5243@srmist.edu.in>
2022-02-28 11:55:06 +0530

    -commit

--- gunpreet.c
+++ gunpreet.c
@@ -7,10 +7,8 @@ int main() {
     int n;
     scanf("%d",&n);
     int grid[n+1],i;
-    grid[0]=0;
-    grid[1]=1;
-    grid[2]=2;
-    grid[3]=4;
+    grid[0]=0;    grid[1]=1;
+    grid[2]=2;    grid[3]=4;
     for(i=4;i<=n;i++){
         grid[i]=grid[i-1]+grid[i-2]+grid[i-3];
     }
